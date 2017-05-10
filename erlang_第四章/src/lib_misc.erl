fd41f10cfd2ef16a19c20dc398aab4b713a79177
笑你妹c <ytx0573@gmail.com>
2017-05-11 07:49:36 +0800

    Quicksort 毕达哥拉斯三元数组 回文构词

--- erlang_第四章/src/lib_misc.erl
+++ erlang_第四章/src/lib_misc.erl
@@ -10,7 +10,8 @@
 -author("johnson").
 
 %% API
--export([for/3, test/0, test1/0, sum/1, test2/0]).
+-export([for/3, test/0, test1/0, sum/1, test2/0, test3/0, qsort/1]).
+-export([pythag/1, perms/1]).
 
 
 %%自实现for循环  两个函数顺序不能错乱, 以后肯定会经常用到的
@@ -23,7 +24,35 @@ test() ->
 
 test1() ->
   for(1, 10, fun(I) -> I * I end).
+test3() ->
+  L = [22, 3, 55, 5, 000, 342, 55, 4, "44"],
+  qsort(L).
 
+
+qsort([]) -> [];
+qsort([Pivot | T]) ->
+  qsort([ X || X <- T, X < Pivot ])
+  ++ [Pivot] ++
+    qsort([ X || X <- T, X >= Pivot]).
+%%  上面代码X和Pivot替换, 则为倒叙排列. 字符串排列最后
+
+
+%%毕达哥拉斯三元数组  下面的表达式如果数字过大, 会导致出结果慢
+pythag(N) ->
+  [ {A, B, C} ||
+    A <- lists:seq(1, N),
+    B <- lists:seq(1, N),
+    C <- lists:seq(1, N),
+    A + B + C =< N,
+    A * A + B * B =:= C * C
+  ].
+
+%%回文构词  填字游戏, 排列出字母的所有组合, 过长导致出结果慢
+perms([]) -> [[]];
+perms(L) ->
+  [ [H | T] || H <- L, T <- perms(L -- [H])].
+
+%%lists:seq/2  意思是从1到N的所有值组成的列表
 
 
 sum([H|T]) -> H + sum(T);
--- erlang_第四章/src/test.erl
+++ erlang_第四章/src/test.erl
@@ -10,7 +10,7 @@
 -author("johnson").
 
 %% API
--export([test/0, test1/0, test2/0, test3/0, map/2]).
+-export([test/0, test1/0, test2/0, test3/0, map/2, test4/0]).
 
 
 %% 列表推导, test 为返回X * 2的元素列表, X的从L中提取
@@ -39,7 +39,12 @@ test3() ->
 %%列表推导 自实现map
 map(Func, L) -> [Func(X) || X <- L].
 
+test4() ->
+  [ X || {a, X} <- [{a, 4}, {b, t}, {c, 5}, "555555", ppp, {a, k}] ].
+
 %% lists:map(fun(X) -> X * 0 end, [1, 2, 4]).
 %% test:map(fun(X) -> X * 0 end, [1, 2, 4]).
+%% test:test4().        [4,k]
+
 
 
